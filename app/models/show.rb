08893b4b4f972fbd99aab5feda8593d89f3500f3
Hunter Sides <hunter.s.1337@gmail.com>
2020-09-10 19:50:14 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,7 +3,7 @@ class Show < ActiveRecord::Base
 
   belongs_to :network
   has_many :characters
-  has_many :actors , through: :characters
+  has_many :actors , through: :shows
   
   def actors_list
     self.actor.collect do |t|
