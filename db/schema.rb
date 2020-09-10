d929f915068255e994f40788ae9cf5429aa8edbc
Hunter Sides <hunter.s.1337@gmail.com>
2020-09-10 19:22:43 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 4) do
+ActiveRecord::Schema.define(version: 9) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.integer "character_id"
+  end
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.string "catchphrase"
+    t.integer "show_id"
+    t.integer "actor_id"
   end
 
   create_table "networks", force: :cascade do |t|
@@ -25,6 +33,8 @@ ActiveRecord::Schema.define(version: 4) do
   create_table "shows", force: :cascade do |t|
     t.string "name"
     t.integer "network_id"
+    t.string "day"
+    t.string "genre"
   end
 
 end
